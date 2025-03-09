25804f02ffb19d31e6a91c5f6c07ee4dc88369d6
승찬 <anstmdcks12@naver.com>
2025-03-09 14:45:26 +0900

    feat: added base configuration

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "GroupChat",
+  dependencies: [
+    .package(url: "https://github.com/airbnb/swift", from: "1.0.8"),
+  ],
+  targets: [
+  ])
